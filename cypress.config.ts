8cef053772840ac97ff82ba3d3b05ca15d52adc8
Israa Shtayeh <israamshtayeh@gmail.com>
2023-11-11 15:14:36 +0200

    first commit

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+module.exports = defineConfig({
+e2e: {
+  baseUrl: 'https://opensource-demo.orangehrmlive.com',
+async setupNodeEvents(on, config)
+{
+// implement node event listeners here
+return require("./cypress/plugins")(on, config);
+},
+specPattern: "cypress/e2e/*.feature",
+},
+});
